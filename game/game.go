8d6fcd29d93872b71339cb34bcde3bf109175806
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2024-09-30 06:12:23 -0300

    chore: melhorando a estrutura

---
+++ game/game.go
@@ -0,0 +1,22 @@
+package game
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Game struct{}
+
+// metodos que a lib espera para funcionar --------------------------------------
+// atualiza a logica do jogo
+func (g *Game) Update() error {
+	return nil
+}
+
+// desenha os objetos na tela
+func (g *Game) Draw(scream *ebiten.Image) {
+	
+}
+
+// tamanaho da tela
+func (g *Game) Layout(outsideWith, outsideHeith int) (screamWith, screamHeith int) {
+	return outsideWith, outsideHeith
+}
+//---------------------------------------------------------------------------------
